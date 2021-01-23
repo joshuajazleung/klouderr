4b8834c6cf2589181971c2693da1e0bece4516a4
Joshua Leung <joshua_leung@sfu.ca>
2021-01-22 20:56:31 -0800

    move dl button to the btm

--- client/src/views/File.vue
+++ client/src/views/File.vue
@@ -29,6 +29,29 @@
 
     <hr>
 
+  <!-- <div class="mt-20" v-if="$i18n.locale === 'en'"> -->
+    <div class="mt-20 mb-20">
+        <h4 class="title title--no-sp is-6">Worldwide IaaS Public Cloud Services Market Jumped 37.3% in 2019</h4>
+
+        <p>
+          We all know the overall cloud services and software business is growing at warp speed, but it can be beneficial to enterprise decision-makers if experts break down the categories (SaaS, IaaS, PaaS and others) for more clarity. 
+
+Industry analyst Gartner Inc. reported Aug. 10 that the worldwide infrastructure as a service (IaaS) market grew a whopping 37.3% in 2019 to total $44.5 billion, up from $32.4 billion in 2018. 
+
+To the surprise of few industry observers, Amazon retained the No. 1 position in the IaaS market in 2019, followed by Microsoft, Alibaba, Google and Tencent.
+
+In 2019, the top five IaaS providers accounted for 80% of the market, up from 77% in 2018. Three-quarters of all IaaS providers had exhibited growth in 2018.
+
+Amazon continued to lead the worldwide IaaS market with an estimated $20 billion of revenue in 2019 and 45% of the total market. Amazon leveraged its No.1 spot in 2018 to build out its capabilities beyond the IaaS layer in the cloud stack and maintain its top position in 2019. 
+
+“Cloud underpins the push to digital business, which remains at the top of CIOs’ agendas,” Sid Nag, research vice president at Gartner, said in a media advisory. “It enables technologies such as the edge, AI, machine learning and 5G, among others. At the end of the day, each of these technologies requires a scalable, elastic and high-capacity infrastructure platform like public cloud IaaS, which is why the market witnessed strong growth.” 
+
+Microsoft remained in the No. 2 position in the IaaS market with more than half of its nearly $8 billion in revenue coming from North America. Microsoft’s IaaS offering grew 57.8% in 2019 as the company leveraged its sales reach and ability to co-sell its Azure offerings with other Microsoft products and services in order to drive adoption.
+
+The dominant IaaS provider in China, Alibaba Cloud, grew 62.4% in 2019 with revenue surpassing $4 billion, up from $2.5 billion in 2018. Alibaba Group will continue to expand its cloud infrastructure business in the coming years and aim to offer cloud-based intelligent solutions to its customers to support their digital transformation process.
+        </p>
+    </div>
+
     <h1 class="title is-5 tracking-wide mt-12 text-center">{{ file.name }}</h1>
 
     <div class="ads mt-10 text-center">
@@ -57,28 +80,6 @@
 
     <div class="ads text-center">
         <Adsense data-ad-client="ca-pub-4679085340013866" data-ad-slot="6511749031"></Adsense>
-    </div>
-  <!-- <div class="mt-20" v-if="$i18n.locale === 'en'"> -->
-    <div class="mt-20 mb-20">
-        <h4 class="title title--no-sp is-6">Worldwide IaaS Public Cloud Services Market Jumped 37.3% in 2019</h4>
-
-        <p>
-          We all know the overall cloud services and software business is growing at warp speed, but it can be beneficial to enterprise decision-makers if experts break down the categories (SaaS, IaaS, PaaS and others) for more clarity. 
-
-Industry analyst Gartner Inc. reported Aug. 10 that the worldwide infrastructure as a service (IaaS) market grew a whopping 37.3% in 2019 to total $44.5 billion, up from $32.4 billion in 2018. 
-
-To the surprise of few industry observers, Amazon retained the No. 1 position in the IaaS market in 2019, followed by Microsoft, Alibaba, Google and Tencent.
-
-In 2019, the top five IaaS providers accounted for 80% of the market, up from 77% in 2018. Three-quarters of all IaaS providers had exhibited growth in 2018.
-
-Amazon continued to lead the worldwide IaaS market with an estimated $20 billion of revenue in 2019 and 45% of the total market. Amazon leveraged its No.1 spot in 2018 to build out its capabilities beyond the IaaS layer in the cloud stack and maintain its top position in 2019. 
-
-“Cloud underpins the push to digital business, which remains at the top of CIOs’ agendas,” Sid Nag, research vice president at Gartner, said in a media advisory. “It enables technologies such as the edge, AI, machine learning and 5G, among others. At the end of the day, each of these technologies requires a scalable, elastic and high-capacity infrastructure platform like public cloud IaaS, which is why the market witnessed strong growth.” 
-
-Microsoft remained in the No. 2 position in the IaaS market with more than half of its nearly $8 billion in revenue coming from North America. Microsoft’s IaaS offering grew 57.8% in 2019 as the company leveraged its sales reach and ability to co-sell its Azure offerings with other Microsoft products and services in order to drive adoption.
-
-The dominant IaaS provider in China, Alibaba Cloud, grew 62.4% in 2019 with revenue surpassing $4 billion, up from $2.5 billion in 2018. Alibaba Group will continue to expand its cloud infrastructure business in the coming years and aim to offer cloud-based intelligent solutions to its customers to support their digital transformation process.
-        </p>
     </div>
     <!-- <div class="mt-20" v-else>
         <h4 class="title title--no-sp is-6">透视腾讯、阿里财报背后的云端新战局</h4>
